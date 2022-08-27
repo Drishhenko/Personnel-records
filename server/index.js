6de548c296541c4dc6f47ad139ffac78544e7b4c
Drishhenko <drishhenko@gmail.com>
2022-08-27 13:39:38 +0300

    added cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const sequelize = require('./database')
 const router = require('./routes')
 const PORT = process.env.PORT
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
